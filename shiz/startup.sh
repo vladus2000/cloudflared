1d8339597ee8c223da758874c1da8937613487af
vladus2000 <docker@matt.land>
2019-02-15 23:29:59 -0600

    fixing listening address

--- shiz/startup.sh
+++ shiz/startup.sh
@@ -1,6 +1,6 @@
 TUNNEL_DNS_ADDRESS=0.0.0.0
 while true
 do
-	cloudflared proxy-dns --port 5053 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query
+	cloudflared proxy-dns --port 5053 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query --address 0.0.0.0
 	sleep 1
 done
